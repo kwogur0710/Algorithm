64405730f98a52eb98157ded7862c930fc8f3b81
kwogur0710 <49198931+kwogur0710@users.noreply.github.com>
2023-06-25 03:11:14 +0900

    [unrated] Title: 옹알이 (2), Time: 0.31 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -0,0 +1,18 @@
+function solution(babbling) {
+    var answer = 0;
+    let i = 0;
+    let lastIndex = 4;
+    let list = ["aya", "ye", "woo", "ma"];
+
+    for(i = 0; i < babbling.length; i++) {
+        let filter = babbling[i];
+        let continuity = false;
+        list.map(item => {
+            if(babbling[i].includes(item.repeat(2))) continuity = true;
+        });
+        filter = filter.replaceAll(/aya|ye|woo|ma/g, "");
+        if(filter==='' && !continuity) answer++;
+    };
+    
+    return answer;
+}
